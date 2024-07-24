081851d70b4affc7bf7b63026a93af34a55f47ea
Light-r4y <116795744+Light-r4y@users.noreply.github.com>
2024-07-24 21:47:59 +0300

    add files

---
+++ CAN-ESPNOW/esp32s2_espnow_master.ino
@@ -0,0 +1,222 @@
+#include "ESP32_NOW.h"
+#include "WiFi.h"
+#include "driver/twai.h"
+
+#include <esp_mac.h>  // For the MAC2STR and MACSTR macros
+
+/* Definitions */
+#define DEBUG 0
+
+#define ESPNOW_WIFI_CHANNEL 6
+
+// Pins used to connect to CAN bus transceiver:
+#define RX_PIN 39  //42
+#define TX_PIN 40  //41
+#define LED_PIN 15
+
+// Interval:
+#define POLLING_RATE_MS 50
+
+
+/* Classes */
+
+// Creating a new class that inherits from the ESP_NOW_Peer class is required.
+
+class ESP_NOW_Broadcast_Peer : public ESP_NOW_Peer {
+public:
+  // Constructor of the class using the broadcast address
+  ESP_NOW_Broadcast_Peer(uint8_t channel, wifi_interface_t iface, const uint8_t *lmk)
+    : ESP_NOW_Peer(ESP_NOW.BROADCAST_ADDR, channel, iface, lmk) {}
+
+  // Destructor of the class
+  ~ESP_NOW_Broadcast_Peer() {
+    remove();
+  }
+
+  // Function to properly initialize the ESP-NOW and register the broadcast peer
+  bool begin() {
+    if (!ESP_NOW.begin() || !add()) {
+      log_e("Failed to initialize ESP-NOW or register the broadcast peer");
+      return false;
+    }
+    return true;
+  }
+
+  // Function to send a message to all devices within the network
+  bool send_message(const uint8_t *data, size_t len) {
+    if (!send(data, len)) {
+      log_e("Failed to broadcast message");
+      return false;
+    }
+    digitalWrite(LED_PIN, LOW);
+    return true;
+  }
+};
+
+/* Global Variables */
+
+static bool driver_installed = false;
+volatile static bool toSend = false;
+
+typedef struct struct_message {
+  int rpm;
+  int speed;
+  int clt;
+  int iat;
+  int afr;
+  int Vbat;
+  int fuel;
+} struct_message;
+
+struct_message myData;
+
+// Create a broadcast peer object
+ESP_NOW_Broadcast_Peer broadcast_peer(ESPNOW_WIFI_CHANNEL, WIFI_IF_STA, NULL);
+
+/* Main */
+
+void setup() {
+  Serial.begin(115200);
+
+  // Initialize configuration structures using macro initializers
+  twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT((gpio_num_t)TX_PIN, (gpio_num_t)RX_PIN, TWAI_MODE_LISTEN_ONLY);
+  twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
+  twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+
+  // Install TWAI driver
+  if (twai_driver_install(&g_config, &t_config, &f_config) == ESP_OK) {
+    Serial.println("Driver installed");
+  } else {
+    Serial.println("Failed to install driver");
+    return;
+  }
+
+  // Start TWAI driver
+  if (twai_start() == ESP_OK) {
+    Serial.println("Driver started");
+  } else {
+    Serial.println("Failed to start driver");
+    return;
+  }
+
+  // Reconfigure alerts to detect frame receive, Bus-Off error and RX queue full states
+  uint32_t alerts_to_enable = TWAI_ALERT_RX_DATA | TWAI_ALERT_ERR_PASS | TWAI_ALERT_BUS_ERROR | TWAI_ALERT_RX_QUEUE_FULL;
+  if (twai_reconfigure_alerts(alerts_to_enable, NULL) == ESP_OK) {
+    Serial.println("CAN Alerts reconfigured");
+  } else {
+    Serial.println("Failed to reconfigure alerts");
+    return;
+  }
+
+  // TWAI driver is now successfully installed and started
+  driver_installed = true;
+
+  delay(1000);
+  // Initialize the Wi-Fi module
+  WiFi.mode(WIFI_STA);
+  WiFi.setChannel(ESPNOW_WIFI_CHANNEL);
+  while (!WiFi.STA.started()) {
+    delay(100);
+  }
+
+  if (DEBUG) {
+    Serial.println("ESP-NOW Start");
+    Serial.println("Wi-Fi parameters:");
+    Serial.println("  Mode: STA");
+    Serial.println("  MAC Address: " + WiFi.macAddress());
+    Serial.printf("  Channel: %d\n", ESPNOW_WIFI_CHANNEL);
+  }
+
+  // Register the broadcast peer
+  if (!broadcast_peer.begin()) {
+    Serial.println("Failed to initialize broadcast peer");
+    Serial.println("Reebooting in 5 seconds...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Trace work
+  memset(&myData, 0, sizeof(myData));
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+
+  Serial.println("Setup complete.");
+}
+
+void loop() {
+  //Broadcast a message to all devices within the network
+
+  if (!driver_installed) {
+    // Driver not installed
+    delay(1000);
+    return;
+  }
+  // Check if alert happened
+  uint32_t alerts_triggered;
+  twai_read_alerts(&alerts_triggered, pdMS_TO_TICKS(POLLING_RATE_MS));
+  twai_status_info_t twaistatus;
+  twai_get_status_info(&twaistatus);
+
+  // Handle alerts
+  if (alerts_triggered & TWAI_ALERT_ERR_PASS) {
+    Serial.println("Alert: TWAI controller has become error passive.");
+  }
+  if (alerts_triggered & TWAI_ALERT_BUS_ERROR) {
+    Serial.println("Alert: A (Bit, Stuff, CRC, Form, ACK) error has occurred on the bus.");
+    Serial.printf("Bus error count: %lu\n", twaistatus.bus_error_count);
+  }
+  if (alerts_triggered & TWAI_ALERT_RX_QUEUE_FULL) {
+    Serial.println("Alert: The RX queue is full causing a received frame to be lost.");
+    Serial.printf("RX buffered: %lu\t", twaistatus.msgs_to_rx);
+    Serial.printf("RX missed: %lu\t", twaistatus.rx_missed_count);
+    Serial.printf("RX overrun %lu\n", twaistatus.rx_overrun_count);
+  }
+
+  // Check if message is received
+  if (alerts_triggered & TWAI_ALERT_RX_DATA) {
+    // One or more messages received. Handle all.
+    twai_message_t message;
+    while (twai_receive(&message, 0) == ESP_OK) {
+      if (DEBUG) { handle_rx_message(message); }
+
+      if (message.identifier == 0x180) {
+        myData.rpm = (message.data[0] << 8 | message.data[1]) / 8;
+        toSend = true;
+      }
+      if (message.identifier == 0x280) {
+        myData.speed = (message.data[4] << 8 | message.data[5]) / 100;
+        toSend = true;
+      }
+      if (message.identifier == 0x551) {
+        myData.clt = message.data[1] - 40;
+        toSend = true;
+      }
+      
+      // Send via ESPNOW
+      if (toSend) {
+        broadcast_peer.send_message((const uint8_t *)&myData, sizeof(myData));
+        toSend = false;
+      }
+
+      //Trace msg
+      if (DEBUG) { Serial.printf("Broadcasting message: rpm= %d, spd= %d, clt= %d\r\n", myData.rpm, myData.speed, myData.clt); }
+    }
+  }
+  digitalWrite(LED_PIN, HIGH);
+}
+
+static void handle_rx_message(twai_message_t &message) {
+  // Process received message
+  if (message.extd) {
+    Serial.println("Message is in Extended Format");
+  } else {
+    Serial.println("Message is in Standard Format");
+  }
+  Serial.printf("ID: %lx\nByte:", message.identifier);
+  if (!(message.rtr)) {
+    for (int i = 0; i < message.data_length_code; i++) {
+      Serial.printf(" %d = %02x,", i, message.data[i]);
+    }
+    Serial.println("");
+  }
+}
---
+++ DISPLAY/esp32c3_display.ino
@@ -0,0 +1,348 @@
+#include "Arduino.h"
+#include <LovyanGFX.hpp>
+#include "CST816D.h"
+#include <lvgl.h>
+#include "lv_conf.h"
+
+#include "ui.h"
+
+#include "ESP32_NOW.h"
+#include "WiFi.h"
+
+#include <esp_mac.h>  // For the MAC2STR and MACSTR macros
+
+#include <vector>
+
+#define BL 3
+
+#define I2C_SDA 4
+#define I2C_SCL 5
+#define TP_INT 0
+#define TP_RST 1
+
+#define TFT_HOR_RES 240
+#define TFT_VER_RES 240
+
+/*LVGL draw into this buffer, 1/10 screen size usually works well. The size is in bytes*/
+#define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 10 * (LV_COLOR_DEPTH / 8))
+uint32_t draw_buf[DRAW_BUF_SIZE / 4];
+
+typedef struct struct_message {
+  int rpm;
+  int speed;
+  int clt;
+  int iat;
+  int afr;
+  int Vbat;
+  int fuel;
+} struct_message;
+
+struct_message myData;
+volatile static bool toReceve = false;
+
+#if LV_USE_LOG != 0
+void my_print(lv_log_level_t level, const char *buf) {
+  LV_UNUSED(level);
+  Serial.println(buf);
+  Serial.flush();
+}
+#endif
+
+
+class LGFX : public lgfx::LGFX_Device {
+
+  lgfx::Panel_GC9A01 _panel_instance;
+  lgfx::Light_PWM _light_instance;
+  lgfx::Bus_SPI _bus_instance;
+
+public:
+  LGFX(void) {
+    {
+      auto cfg = _bus_instance.config();
+
+      cfg.spi_host = SPI2_HOST;
+      cfg.spi_mode = 0;
+      cfg.freq_write = 80000000;
+      cfg.freq_read = 20000000;
+      cfg.spi_3wire = true;
+      cfg.use_lock = true;
+      cfg.dma_channel = SPI_DMA_CH_AUTO;
+      cfg.pin_sclk = 6;
+      cfg.pin_mosi = 7;
+      cfg.pin_miso = -1;
+      cfg.pin_dc = 2;
+
+      _bus_instance.config(cfg);
+      _panel_instance.setBus(&_bus_instance);
+    }
+
+    {
+      auto cfg = _panel_instance.config();
+
+      cfg.pin_cs = 10;
+      cfg.pin_rst = -1;
+      cfg.pin_busy = -1;
+
+      cfg.memory_width = 240;
+      cfg.memory_height = 240;
+      cfg.panel_width = 240;
+      cfg.panel_height = 240;
+      cfg.offset_x = 0;
+      cfg.offset_y = 0;
+      cfg.offset_rotation = 0;
+      cfg.dummy_read_pixel = 8;
+      cfg.dummy_read_bits = 1;
+      cfg.readable = false;
+      cfg.invert = true;
+      cfg.rgb_order = false;
+      cfg.dlen_16bit = false;
+      cfg.bus_shared = false;
+
+      _panel_instance.config(cfg);
+    }
+
+    {
+      auto cfg = _light_instance.config();
+
+      cfg.pin_bl = BL;
+      cfg.invert = false;
+      cfg.freq = 44100;
+      cfg.pwm_channel = 7;
+      _light_instance.config(cfg);
+      _panel_instance.setLight(&_light_instance);
+    }
+
+    setPanel(&_panel_instance);
+  }
+};
+
+LGFX tft;
+CST816D touch(I2C_SDA, I2C_SCL, TP_RST, TP_INT);
+
+#if LV_USE_LOG != 0
+/* Serial debugging */
+void my_print(lv_log_level_t level, const char *file, uint32_t line, const char *fn_name, const char *dsc) {
+  Serial.printf("%s(%s)@%d->%s\r\n", file, fn_name, line, dsc);
+  Serial.flush();
+}
+#endif
+
+/* LVGL calls it when a rendered image needs to copied to the display*/
+void my_disp_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map) {
+
+  uint32_t w = lv_area_get_width(area);
+  uint32_t h = lv_area_get_height(area);
+  lv_draw_sw_rgb565_swap(px_map, w * h);
+
+  if (tft.getStartCount() == 0) {
+    tft.endWrite();
+  }
+  tft.pushImageDMA(area->x1, area->y1, area->x2 - area->x1 + 1, area->y2 - area->y1 + 1, (uint16_t *)px_map);
+
+  lv_display_flush_ready(disp);
+}
+
+/*Read the touchpad*/
+void my_touchpad_read(lv_indev_t *indev, lv_indev_data_t *data) {
+
+  bool touched;
+  uint8_t gesture;
+  uint16_t touchX, touchY;
+
+  touched = touch.getTouch(&touchX, &touchY, &gesture);
+  if (!touched) {
+    data->state = LV_INDEV_STATE_RELEASED;
+  } else {
+    data->state = LV_INDEV_STATE_PRESSED;
+
+    data->point.x = touchX;
+    data->point.y = touchY;
+  }
+}
+
+// ================================================
+// ESPNOW BROADCAST SLAVE RECIEVER
+
+#define ESPNOW_WIFI_CHANNEL 6
+
+class ESP_NOW_Peer_Class : public ESP_NOW_Peer {
+public:
+  // Constructor of the class
+  ESP_NOW_Peer_Class(const uint8_t *mac_addr, uint8_t channel, wifi_interface_t iface, const uint8_t *lmk)
+    : ESP_NOW_Peer(mac_addr, channel, iface, lmk) {}
+
+  // Destructor of the class
+  ~ESP_NOW_Peer_Class() {}
+
+  // Function to register the master peer
+  bool add_peer() {
+    if (!add()) {
+      log_e("Failed to register the broadcast peer");
+      return false;
+    }
+    return true;
+  }
+
+  // Function to print the received messages from the master
+  void onReceive(const uint8_t *data, size_t len, bool broadcast) {
+    Serial.printf("Received a message from master " MACSTR " (%s)\n", MAC2STR(addr()), broadcast ? "broadcast" : "unicast");
+    Serial.printf("  Message: %s\n", (char *)data);
+    memcpy(&myData, data, sizeof(myData));
+    toReceve = true;
+    // lv_label_set_text_fmt(ui_Label2, "%s", (char *)data);
+  }
+};
+
+/* Global Variables */
+
+// List of all the masters. It will be populated when a new master is registered
+std::vector<ESP_NOW_Peer_Class> masters;
+
+/* Callbacks */
+
+// Callback called when an unknown peer sends a message
+void register_new_master(const esp_now_recv_info_t *info, const uint8_t *data, int len, void *arg) {
+  if (memcmp(info->des_addr, ESP_NOW.BROADCAST_ADDR, 6) == 0) {
+    Serial.printf("Unknown peer " MACSTR " sent a broadcast message\n", MAC2STR(info->src_addr));
+    Serial.println("Registering the peer as a master");
+
+    ESP_NOW_Peer_Class new_master(info->src_addr, ESPNOW_WIFI_CHANNEL, WIFI_IF_STA, NULL);
+
+    masters.push_back(new_master);
+    if (!masters.back().add_peer()) {
+      Serial.println("Failed to register the new master");
+      return;
+    }
+  } else {
+    // The slave will only receive broadcast messages
+    log_v("Received a unicast message from " MACSTR, MAC2STR(info->src_addr));
+    log_v("Igorning the message");
+  }
+}
+
+// ================================================
+
+void setup() {
+
+  tft.init();
+  tft.initDMA();
+  tft.setBrightness(128);
+
+  touch.begin();
+
+  String LVGL_Arduino = "Hello Arduino! ";
+  LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+
+
+  Serial.begin(115200);
+  Serial.println(LVGL_Arduino);
+
+  lv_init();
+
+  /*Set a tick source so that LVGL will know how much time elapsed. */
+  lv_tick_set_cb(millis);
+
+  /* register print function for debugging */
+#if LV_USE_LOG != 0
+  lv_log_register_print_cb(my_print);
+#endif
+
+  lv_display_t *disp;
+#if LV_USE_TFT_ESPI
+  /*TFT_eSPI can be enabled lv_conf.h to initialize the display in a simple way*/
+  disp = lv_tft_espi_create(TFT_HOR_RES, TFT_VER_RES, draw_buf, sizeof(draw_buf));
+#else
+  /*Else create a display yourself*/
+  disp = lv_display_create(TFT_HOR_RES, TFT_VER_RES);
+  lv_display_set_flush_cb(disp, my_disp_flush);
+  lv_display_set_buffers(disp, draw_buf, NULL, sizeof(draw_buf), LV_DISPLAY_RENDER_MODE_PARTIAL);
+#endif
+
+  /*Initialize the (dummy) input device driver*/
+  lv_indev_t *indev = lv_indev_create();
+  lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER); /*Touchpad should have POINTER type*/
+  lv_indev_set_read_cb(indev, my_touchpad_read);
+
+  ui_init();
+
+  // =================================
+  //ESPNOW
+
+  // Initialize the Wi-Fi module
+  WiFi.mode(WIFI_STA);
+  WiFi.setChannel(ESPNOW_WIFI_CHANNEL);
+  while (!WiFi.STA.started()) {
+    delay(100);
+  }
+
+  Serial.println("ESP-NOW Example - Broadcast Slave");
+  Serial.println("Wi-Fi parameters:");
+  Serial.println("  Mode: STA");
+  Serial.println("  MAC Address: " + WiFi.macAddress());
+  Serial.printf("  Channel: %d\n", ESPNOW_WIFI_CHANNEL);
+
+  // Initialize the ESP-NOW protocol
+  if (!ESP_NOW.begin()) {
+    Serial.println("Failed to initialize ESP-NOW");
+    Serial.println("Reeboting in 5 seconds...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Register the new peer callback
+  ESP_NOW.onNewPeer(register_new_master, NULL);
+
+  Serial.println("Setup complete. Waiting for a master to broadcast a message...");
+
+  // Serial.println("Setup done");
+}
+
+String s;
+
+void loop(void) {
+  lv_task_handler(); /* let the GUI do its work */
+  delay(5);          /* let this time pass */
+
+  if (toReceve) updateValue();
+  // while (Serial.available()) {
+  //   s = Serial.readStringUntil('\n');
+  //   // int x = s.toInt();
+  //   // lv_arc_set_value(ui_ArcRpm, x);
+  //   // lv_label_set_text_fmt(ui_Label2, "%s", s);
+  // }
+}
+
+void updateValue() {
+  lv_arc_set_value(ui_ArcRpm, myData.rpm);
+  lv_label_set_text_fmt(ui_rpm, "%d", myData.rpm);
+  if (myData.rpm > 6000) {
+    lv_obj_set_style_arc_color(ui_ArcRpm, lv_color_hex(0xFF0000), LV_PART_INDICATOR | LV_STATE_DEFAULT);
+  } else if (myData.rpm > 5000) {
+    lv_obj_set_style_arc_color(ui_ArcRpm, lv_color_hex(0xE6FF00), LV_PART_INDICATOR | LV_STATE_DEFAULT);
+  } else {
+    lv_obj_set_style_arc_color(ui_ArcRpm, lv_color_hex(0xFFFFFF), LV_PART_INDICATOR | LV_STATE_DEFAULT);
+  }
+  lv_arc_set_value(ui_ArcClt, myData.clt);
+  lv_label_set_text_fmt(ui_Clt, "%d", myData.clt);
+  if (myData.clt > 105) {
+    lv_obj_set_style_arc_color(ui_ArcClt, lv_color_hex(0xFF0000), LV_PART_INDICATOR | LV_STATE_DEFAULT);
+  }
+  else if (myData.clt > 50) {
+    lv_obj_set_style_arc_color(ui_ArcClt, lv_color_hex(0x00FFBB), LV_PART_INDICATOR | LV_STATE_DEFAULT);
+  }
+  else {
+    lv_obj_set_style_arc_color(ui_ArcClt, lv_color_hex(0x2ED4FF), LV_PART_INDICATOR | LV_STATE_DEFAULT);
+  }
+  lv_label_set_text_fmt(ui_Spd, "%d", myData.speed);
+
+  toReceve = false;
+}
+
+void setBrightness(lv_event_t *e) {
+  int v = lv_slider_get_value(ui_Slider1);
+  tft.setBrightness(v);
+
+  // lv_label_set_text_fmt(ui_Label1, "Brig set: %d", v);
+
+  Serial.println(v);
+}
